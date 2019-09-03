fb5f5599ebd20d7a431952e6beaaa0489f811142
Gianluca Fiore <gianlucafiore@papersounds.eu>
2019-09-03 21:30:12 +0200

    licensing Go geocoder

--- go_geocode.go
+++ go_geocode.go
@@ -4,6 +4,11 @@ package main
 //
 // Copyright (c) 2019, Gianluca Fiore
 //
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
 ////////////////////////////////////////////////////////////////////////////////
 
 import (
